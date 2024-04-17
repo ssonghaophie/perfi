d1b1adac9c3b5aa832667b3856aa3de8dd2b09d4
Sophie Song <ssong25@smith.edu>
2024-04-17 09:05:32 -0400

    edit read_boa function

--- R/perfi.R
+++ R/perfi.R
@@ -1,6 +1,6 @@
 #' @title Reading transaction spreadsheet and output personal finance statistics
 #' @description
-#' @importFrom utils read.csv
+#' @importFrom utils read.csv dplyr mutate
 #' @export
 #'
 #'
@@ -9,7 +9,10 @@
 read_boa <- function(data_url, ...) {
   # read csv
   data <- utils::read.csv(data_url, skip = 5)
-  # drop unnecessary row
-  data <- data[-1,]
+  # drop unnecessary row, column
+  data <- data[-1, -4]
+  # create a column for expenditure, deposit
+  data <- data |>
+    dplyr::mutate(Status = ifelse(Amount < 0, "Expenditure", "Deposit"))
   return(data)
 }
